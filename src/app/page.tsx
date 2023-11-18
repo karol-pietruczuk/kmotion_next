052b4ef9bbc600a18df13221a4b6cc15d9fb3ce7
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2023-11-18 09:17:54 +0100

    Ended Service Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import Image from 'next/image'
 import './page.css';
+import useWindowDimensions from './hooks/useWindowDimension';
 
 export default function Home() {
+    const { width } = useWindowDimensions();
     return (
         <main className='kmotion-website'>
             <section className='logo-section'>
@@ -16,7 +20,7 @@ export default function Home() {
                         className='logo-section__header--logo'
                         src="/kmotion.svg"
                         alt="KMotion Logo"
-                        width={100}
+                        width={width !== undefined ? (width > 460 ? (width > 1150 ? 0.1 * width : 0.2 * width) : 0.3 * width) : 100}
                         height={24}
                         priority
                     />
@@ -29,111 +33,53 @@ export default function Home() {
                     </h2>
                 </header>
                 <article className='service-section__article'>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
-                    <div className='service-section__article--service'>
-                        <div className='service-section__article--service-hex'></div>
-                    </div>
+                        <div className='service-section__article--service'>
+                            <div className='service-section__article--service-hex'>
+                                <p className='service-section__article--service-hex--desc'>Programowanie sterowników, m.in. Allen Bradley, Eaton, Mitsubishi, Siemens</p>
+                            </div>
+                        </div>
+                        <div className='service-section__article--service'>
+                            <div className='service-section__article--service-hex'>
+                                <p className='service-section__article--service-hex--desc'>Tworzenie wizualizacji na panelach HMI, m.in. Galileo, Allen Bradley</p>
+                            </div>
+                        </div>
+                        <div className='service-section__article--service'>
+                            <div className='service-section__article--service-hex'>
+                                <p className='service-section__article--service-hex--desc'>Usuwanie usterek maszyn</p>
+                            </div>
+                        </div>
+                        <div className='service-section__article--service'>
+                            <div className='service-section__article--service-hex'>
+                                <p className='service-section__article--service-hex--desc'>Naprawa/przeróbki sterowania</p>
+                            </div>
+                        </div>
+                        <div className='service-section__article--service'>
+                            <div className='service-section__article--service-hex'>
+                                <p className='service-section__article--service-hex--desc'>Tworzenie stron internetowych</p>
+                            </div>
+                        </div>
+                        <div className='service-section__article--service'>
+                            <div className='service-section__article--service-hex'>
+                            <p className='service-section__article--service-hex--desc'>Prefabrykacja szaf sterowniczych</p>
+                            </div>
+                        </div>
+                        <div className='service-section__article--service'>
+                            <div className='service-section__article--service-hex'>
+                                <p className='service-section__article--service-hex--desc'>Programowanie i naprawa uszkodzonych przemienników częstotliwości, np. Delta, Danfoss, Omron, Lenze, Altivar</p>
+                            </div>
+                        </div>
+                        <div className='service-section__article--service'>
+                            <div className='service-section__article--service-hex'>
+                                <p className='service-section__article--service-hex--desc'>Implementacja nowych systemów sterowania</p>
+                            </div>
+                        </div>
+                        <div className='service-section__article--service'>
+                            <div className='service-section__article--service-hex'>
+                                <p className='service-section__article--service-hex--desc'>Tworzenie schematów elektrycznych</p>
+                            </div>
+                        </div>
                 </article>
             </section>
         </main>
     )
 }
-
-
-// import Image from 'next/image'
-//
-// export default function Home() {
-//     return (
-//         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-//                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300
-// bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30
-// dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"> Get
-// started by editing&nbsp; <code className="font-mono font-bold">src/app/page.tsx</code> </p> <div className="fixed
-// bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black
-// dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"> <a className="pointer-events-none flex place-items-center
-// gap-2 p-8 lg:pointer-events-auto lg:p-0"
-// href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-// target="_blank" rel="noopener noreferrer"> By{' '} <Image src="/kmotion.svg" alt="Vercel Logo"
-// className="dark:invert" width={100} height={24} priority/> </a></div> </div> <div className="relative flex
-// place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full
-// before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute
-// after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200
-// after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent
-// before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff]
-// after:dark:opacity-40 before:lg:h-[360px] z-[-1]"><Image className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]
-// dark:invert" src="/next.svg" alt="Next.js Logo" width={180} height={37} priority/></div> <div className="mb-32 grid
-// text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"><a
-// href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-// className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300
-// hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30" target="_blank" rel="noopener
-// noreferrer"><h2 className={`mb-3 text-2xl font-semibold`}> Docs{' '} <span className="inline-block
-// transition-transform group-hover:translate-x-1 motion-reduce:transform-none"> -&gt; </span> </h2> <p className={`m-0
-// max-w-[30ch] text-sm opacity-50`}> Find in-depth information about Next.js features and API. </p></a> <a
-// href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-// className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300
-// hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30" target="_blank" rel="noopener
-// noreferrer"><h2 className={`mb-3 text-2xl font-semibold`}> Learn{' '} <span className="inline-block
-// transition-transform group-hover:translate-x-1 motion-reduce:transform-none"> -&gt; </span> </h2> <p className={`m-0
-// max-w-[30ch] text-sm opacity-50`}> Learn about Next.js in an interactive course with&nbsp;quizzes! </p></a> <a
-// href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-// className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300
-// hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30" target="_blank" rel="noopener
-// noreferrer"><h2 className={`mb-3 text-2xl font-semibold`}> Templates{' '} <span className="inline-block
-// transition-transform group-hover:translate-x-1 motion-reduce:transform-none"> -&gt; </span> </h2> <p className={`m-0
-// max-w-[30ch] text-sm opacity-50`}> Explore the Next.js 13 playground. </p></a> <a
-// href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-// className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300
-// hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30" target="_blank" rel="noopener
-// noreferrer"><h2 className={`mb-3 text-2xl font-semibold`}> Deploy{' '} <span className="inline-block
-// transition-transform group-hover:translate-x-1 motion-reduce:transform-none"> -&gt; </span> </h2> <p className={`m-0 max-w-[30ch] text-sm opacity-50`}> Instantly deploy your Next.js site to a shareable URL with Vercel. </p></a></div> </main>) }
